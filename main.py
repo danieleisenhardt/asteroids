cae6a9c42a3840289c4dfa20ae0dadf97aa9fde5
Daniel Eisenhardt <eisenhardtdaniel@protonmail.com>
2024-08-29 20:37:26 +0200

    it has a ticking clock

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -14,10 +16,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
-
-    print("Starting asteroids!")
-    print(f"Screen width: " + str(SCREEN_WIDTH))
-    print(f"Screen height: " + str(SCREEN_HEIGHT))
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
